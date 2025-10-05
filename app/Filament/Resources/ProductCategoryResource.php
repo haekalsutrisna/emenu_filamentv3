fff0d1206dacd295c2b63845ab1225153fad1298
Muhammad Haekal Sutrisna <78276395+haekalsutrisna@users.noreply.github.com>
2025-10-05 14:38:11 +0700

    1

--- app/Filament/Resources/ProductCategoryResource.php
+++ app/Filament/Resources/ProductCategoryResource.php
@@ -65,7 +65,9 @@ class ProductCategoryResource extends Resource
                     ->relationship('user', 'name'),
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
