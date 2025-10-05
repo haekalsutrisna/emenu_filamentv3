e8caf390554bab5828247ef5e6868f9db5311eed
Muhammad Haekal Sutrisna <78276395+haekalsutrisna@users.noreply.github.com>
2025-10-05 09:59:49 +0700

    update

--- database/migrations/2025_10_05_025604_create_product_categories_table.php
+++ database/migrations/2025_10_05_025604_create_product_categories_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('product_categories', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->string('slug');
             $table->timestamps();
         });
     }
