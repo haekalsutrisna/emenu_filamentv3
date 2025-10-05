69f5d5a1d4931d384b73ad62aad188a235f49c9b
Muhammad Haekal Sutrisna <78276395+haekalsutrisna@users.noreply.github.com>
2025-10-05 10:01:09 +0700

    1

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function productCategories()
+    {
+        return $this->hasMany(ProductCategory::class);
+    }
 }
