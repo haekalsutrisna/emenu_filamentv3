01a4f82ecf83c3adde60cb48c8a43a05b1df102a
Muhammad Haekal Sutrisna <78276395+haekalsutrisna@users.noreply.github.com>
2025-10-05 10:59:58 +0700

    1

---
+++ app/Models/Subscription.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Subscription extends Model
+{
+    use softDeletes;
+    protected $fillable = [
+        'user_id',
+        'end_date',
+        'is_active',
+        'plan',
+    ];
+}
