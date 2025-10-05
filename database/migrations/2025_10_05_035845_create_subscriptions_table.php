251f1fbc36ad32a8caf2ab4c51c63c493cec976a
Muhammad Haekal Sutrisna <78276395+haekalsutrisna@users.noreply.github.com>
2025-10-05 11:00:51 +0700

    1

--- database/migrations/2025_10_05_035845_create_subscriptions_table.php
+++ database/migrations/2025_10_05_035845_create_subscriptions_table.php
@@ -14,7 +14,8 @@ return new class extends Migration
         Schema::create('subscriptions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->dateTime
+            $table->dateTime('end_date');
+            $table->boolean('is_active')->default(false);
             $table->timestamps();
         });
     }
