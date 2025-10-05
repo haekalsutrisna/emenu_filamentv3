ee708f6766b835526c40fe1e1bdad5f92d7dd335
Muhammad Haekal Sutrisna <78276395+haekalsutrisna@users.noreply.github.com>
2025-10-05 10:47:30 +0700

    1

--- app/Models/TransactionDetail.php
+++ app/Models/TransactionDetail.php
@@ -17,4 +17,9 @@ class TransactionDetail extends Model
     {
         return $this->belongsTo(Transaction::class);
     }   
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }   
 }
