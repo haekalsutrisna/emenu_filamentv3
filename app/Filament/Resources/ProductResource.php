b3026e129138bb4a62a36b2da33f3de8cf9720ea
Muhammad Haekal Sutrisna <78276395+haekalsutrisna@users.noreply.github.com>
2025-10-06 13:04:30 +0700

    1

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\ProductResource\Pages;
 use App\Filament\Resources\ProductResource\RelationManagers;
 use App\Models\Product;
+use App\Models\ProductCategory;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
