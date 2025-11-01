c20048528cb9debf46f6ec0e6c7489511100791c
Muhammad Haekal Sutrisna <78276395+haekalsutrisna@users.noreply.github.com>
2025-11-02 00:49:18 +0700

    delete

--- app/Filament/Resources/SubscriptionResource.php
+++ app/Filament/Resources/SubscriptionResource.php
@@ -78,6 +78,7 @@ class SubscriptionResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
