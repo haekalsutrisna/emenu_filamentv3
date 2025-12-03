0366f2a99958a8f11d0e305677d36ef1fcd03a14
Muhammad Haekal Sutrisna <78276395+haekalsutrisna@users.noreply.github.com>
2025-12-03 11:21:16 +0700

    fix bug registration

--- app/Filament/Pages/Auth/Register.php
+++ app/Filament/Pages/Auth/Register.php
@@ -5,7 +5,4 @@ namespace App\Filament\Pages\Auth;
 use Filament\Pages\Page;
 use Filament\Pages\Auth\Register as BaseRegister;
 
-class Register extends Page
-{
-
-}
+class Register extends BaseRegister{}
