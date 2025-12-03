2c784b7c0c366b36d45156e96a1ce0beb6b4a164
Muhammad Haekal Sutrisna <78276395+haekalsutrisna@users.noreply.github.com>
2025-12-03 16:01:57 +0700

    hubungkan front end dengan backend blade

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FrontendController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/{username}', [FrontendController::class, 'index'])->name('index');
